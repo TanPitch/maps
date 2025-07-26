aa097bd2a4fc2baf64acd0aa650465f75c47b01d
TanPitch <47109201+TanPitch@users.noreply.github.com>
2025-07-26 12:24:59 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+TODO:
+[x] filter lists
+[x] pin data -> get from xlsx
+[ ] search function
+[x] drop down for each quick search -> i.e. hospital -> hospital, clinic, pharmarcy, dentist
+[x] dim opacity of non selected pin
+[ ] calculate distance, ETA to each search pin -> show nearest pin
+[x] flood map
+[ ] add custom data for more updated pin
+[ ] click for reverse geo
+
+*/
+
+import * as modal from "./modules/modal.js";
+import * as q_search from "./modules/q_search.js";
+import * as map from "./modules/map.js";
+import * as overlay from "./modules/overlay.js";
+import * as flood from "./modules/flood.js";
